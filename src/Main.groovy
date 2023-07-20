e3913af29647f5a5d459734d01ddf9c88d673e52
olgakargan <108974139+olgakargan@users.noreply.github.com>
2023-07-20 08:44:04 +0400

    Update Main.groovy

--- src/Main.groovy
+++ src/Main.groovy
@@ -1,3 +1,16 @@
-static void main(String[] args) {
-  println "Hello world!"
+def countElements(List list) {
+    Map result = [:]
+
+    list.each { num ->
+        result[num] = result.containsKey(num) ? result[num] + 1 : 1
+    }
+
+    return result
+}
+
+def arr = [1, 3, 4, 5, 1, 5, 4]
+def counts = countElements(arr)
+
+counts.each { key, value ->
+    println(key + " : " + value)
 }
